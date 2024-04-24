6ceddc7d10ca9a10a2da75ea4803c58ad1c14ad9
Samyam <139134689+samyam81@users.noreply.github.com>
2024-04-24 15:34:28 +0545

    dynamicFIBOwithHASHMAP

--- fibo.java
+++ fibo.java
@@ -1,17 +1,24 @@
-public class fibo{ 
-     public static void main(String[] args) {
-        System.out.println(fiborev(2));
-        System.out.println(fiborev(7));
-        System.out.println(fiborev(13));
-        System.out.println(fiborev(40));
-     }
+import java.util.HashMap;
 
-    private static int fiborev(int n) {
-        if (n<=2) {
+public class fibo{ 
+    public static void main(String[] args) {
+        System.out.println(fiborev(2, new HashMap<>()));
+        System.out.println(fiborev(7, new HashMap<>()));
+        System.out.println(fiborev(13, new HashMap<>()));
+        System.out.println(fiborev(50, new HashMap<>()));
+    }
+
+    private static long fiborev(int n, HashMap<Integer, Long> memo) {
+        if (n <= 2) {
             return 1;
         }
-        else{
-            return fiborev(n-1)+fiborev(n-2);
+        
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        } else {
+            long fib = fiborev(n - 1, memo) + fiborev(n - 2, memo);
+            memo.put(n, fib);
+            return fib;
         }
-    }   
+    }
 }

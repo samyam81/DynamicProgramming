93e2e85d9910f988d3518001da178b7517be2d47
Samyam <139134689+samyam81@users.noreply.github.com>
2024-04-28 14:17:04 +0545

    dynamic

--- canConstructMain.kt
+++ canConstructMain.kt
@@ -1,15 +1,21 @@
 fun main() {
     println(canConstruct("abcdef", arrayOf("abc", "def")))
-
+    println(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeee", arrayOf("aaaa", "eee")))
 }
 
-fun canConstruct(target:String,alpha:Array<String>):Boolean?{
-    if (target=="") return true
-    alpha.forEach{ word:String->
-        if (target.indexOf(word)==0){
-            val suffix:String=target.substring(word.length)
-            if (canConstruct(suffix,alpha)==true) return true
+fun canConstruct(target: String, alpha: Array<String>, samyam: MutableMap<String, Boolean> = mutableMapOf()): Boolean? {
+    if (target == "") return true
+    if (samyam.containsKey(target)) return samyam[target]
+
+    alpha.forEach { word ->
+        val suffix: String = target.substring(word.length)
+        if (canConstruct(suffix, alpha, samyam) == true) {
+            samyam[target] = true
+            return true
         }
     }
+
+    samyam[target] = false
     return false
- }
+}
+
